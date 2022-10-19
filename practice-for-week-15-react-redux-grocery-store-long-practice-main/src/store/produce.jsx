a5f811ea5dbd2863d806c3e7205a153999e923f5
shihyulung <shihyulung@yahoo.com.tw>
2022-10-19 14:32:10 -0700

    produce

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.jsx
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.jsx
@@ -1,11 +1,26 @@
+import produceData from "../mockData/produce.json"
 
 const produceReducer = (state={}, action) =>{
+  const newState = {...Object.freeze(state)};
 
   switch(action.type){
-
+    case POPULATE:
+      action.produce.forEach((item)=>{
+        newState[item.id] = item;
+      })
+      return newState;
     default:
-      return state;
+      return newState;
   }
 }
+
+const POPULATE = "POPULATE"
+export const populateProduce = () =>{
+  return ({
+    type: POPULATE,
+    produce: produceData
+  })
+}
+
 
 export default produceReducer;
