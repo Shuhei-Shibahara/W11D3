570b56d54584cb5cc15f5e4f768231f118696a77
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-19 14:01:55 -0700

    phase 2

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import configureStore from './store';
 
+const store = configureStore();
+
+window.store = store
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
